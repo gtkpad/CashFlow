799c605fbe821be21111f9cae5d219b9b39a142d
Gabriel Padilha <gabrielvargaspadilha@gmail.com>
2026-03-04 18:56:08 -0300

    Switch `row_version` to `xmin` as the concurrency token in `daily_summary` table. Update migration and model snapshot. Add `.gitignore` updates.

--- src/CashFlow.Consolidation.API/Migrations/20260304214902_SwitchToXminRowVersion.Designer.cs
+++ src/CashFlow.Consolidation.API/Migrations/20260304214902_SwitchToXminRowVersion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CashFlow.Consolidation.API.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CashFlow.Consolidation.API.Migrations
 {
     [DbContext(typeof(ConsolidationDbContext))]
-    partial class ConsolidationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260304214902_SwitchToXminRowVersion")]
+    partial class SwitchToXminRowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
